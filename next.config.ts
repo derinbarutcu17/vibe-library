d35d0e1d16d3991e18358ee064b7578aefb19fb7
Derin <derinbarutcu@gmail.com>
2026-01-29 17:26:15 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: isProd ? 'export' : undefined,
-  basePath: isProd ? '/vibe-library' : '',
-  assetPrefix: isProd ? '/vibe-library/' : '',
   images: {
     unoptimized: true,
   },
